96ddc4ccc062a961c3ea8e2dc8b44d7e3bc36464
David Vloedman <djvloedman@gmail.com>
2019-09-25 14:20:15 -0700

    Finished 3rd question game

--- js/numberGame.js
+++ js/numberGame.js
@@ -9,8 +9,8 @@ var score = 0;
 var targetNumber = Math.floor((Math.random() * 10) + 1);
 var guess = parseInt(prompt("Guess a number between 1 and 10!"));
 var wasCorrect = false;
-console.log(targetNumber);
-console.log(guess);
+// console.log(targetNumber);
+// console.log(guess);
 
 
 for (var i = 1; i < 4; i++){
@@ -26,7 +26,11 @@ for (var i = 1; i < 4; i++){
     guess = parseInt(prompt('Try again!'));
 };
 
+
+
 if(!wasCorrect) alert('All out of attempts!');
+
+console.log(score);
 
 alert('Answer questions with yes or no!');
 
@@ -37,12 +41,16 @@ if(prompt('Did David go to school?') === 'yes'){
     alert('Incorrect');
 };
 
+console.log(score);
+
 if(prompt('Did David work as a cable guy?') === 'yes'){
     alert('Correct');
     score++
 } else {
     alert('Incorrect');
 };
+
+console.log(score);
 
 if(prompt('Did David go to Sheldon Highschool?') === 'yes'){
     alert('Correct');
@@ -51,13 +59,14 @@ if(prompt('Did David go to Sheldon Highschool?') === 'yes'){
     alert("Incorrect");
 };
 
-if(prompt('Does David like Tennis?') !== 'no'){
+if(prompt('Does David like Tennis?') === 'no'){
     alert('Correct');
     score++;
 } else {
     alert('Incorrect');
 };
 
+console.log(score);
 
 var answerKey = [
     8,
@@ -71,13 +80,25 @@ var answerKey = [
 alert('What numbers are greater than 7, but less than 13? Give two answers.');
 
 
+var attempts = 0;
 
 
-
-for(var i = 1; i < 6; i++){
+while(attempts < 6){
     var answer1 = parseInt(prompt('First answer?'));
-    var answer2 = parseInt(prompt(''))
-}
+    var answer2 = parseInt(prompt('Second answer?'));
+    var correctAnswers = 0;
+    for(var a = 0; a < 5; a++){
+        if(answer1 === answerKey[a]) correctAnswers++;
+        if(answer2 === answerKey[a]) correctAnswers++
+    }
+
+    if(correctAnswers === 2){
+        alert('Congratulations! Those are correct');
+        break;
+    }
+    alert('Try again!');
+    attempts++;
+};
 
 alert('Your score is ' + score + 'out of 7');
 
