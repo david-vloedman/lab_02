b7f1a4eff2e9f5d79125f8379252de980552ad8d
David Vloedman <djvloedman@gmail.com>
2019-09-26 08:26:08 -0700

    Added some config files, removed comments

--- js/app.js
+++ js/app.js
@@ -1,76 +1,117 @@
-// 'use strict'
+'using strict'
+
+var userName = prompt('What is your name?');
+alert('Welcome ' + userName);
+
+var score = 0;
+
+var targetNumber = Math.floor((Math.random() * 10) + 1);
+var guess = parseInt(prompt('Guess a number between 1 and 10!'));
+var wasCorrect = false;
 
 
 
-// var answerKey = [
-//     true,
-//     true,
-//     true,
-//     false
-// ];
+for (var i = 0; i < 4; i++){
+    if(guess === targetNumber){
+        alert('You are correct!');
+        wasCorrect = true;
+        score++;
+    break;
+    }
 
-// function welcomeSequence(){
-//     var userName = prompt('What is your name?');
-//     alert('Welcome ' + userName + '!');
-// }; 
-
-// function startQuiz(){
-//     askA();
-//     askB();
-//     askC();
-//     askD();    
-
-// };
- 
-
-
-// function askA(){
-//     var response = prompt('Is my name David?').toLowerCase();
-//     //console.log(response[0]);
-//     if (sanitizeReponse(response) === answerKey[0]) alert('Correct!') 
-//     else alert('Incorrect');
-// };
-
-// function askB(){
-//     var response = prompt('Did I go to school?').toLowerCase();
-//     if (sanitizeReponse(response) === answerKey[1]) alert('Correct!');
-//     else alert('Incorrect');
-// };
-
-// function askC(){
-//     var response = prompt('Do I want to finish code fellows?').toLowerCase();
-//     if (sanitizeReponse(response) === answerKey[2]) alert('Correct!'); 
-//     else alert('Incorrect');
-// };
-
-// function askD(){
-//     var response = prompt('Am I a sword swallower?').toLowerCase();
-//     if (sanitizeReponse(response) === answerKey[3]) alert('Correct!'); 
-//     else alert('Incorrect');
-// };
+    if(guess < targetNumber) alert('Too low!');
+    if(guess > targetNumber) alert('Too High!');
+    guess = parseInt(prompt('Try again!'));
+};
 
 
 
+if(!wasCorrect) alert('All out of attempts!');
 
 
-// function sanitizeReponse(reponse){
-//     var dirty = reponse;       
-//     if(dirty[0] === 'y') return true;
-//     if(dirty[0] === 'n') return false;
 
-//     instructionPrompt();
+alert('Answer questions with yes or no!');
+
+if(prompt('Did David go to school?') === 'yes'){
+    alert('Correct');
+    score++;
+} else {
+    alert('Incorrect');
+};
+
+
+
+if(prompt('Did David work as a cable guy?') === 'yes'){
+    alert('Correct');
+    score++;
+} else {
+    alert('Incorrect');
+};
+
+
+
+if(prompt('Did David go to Sheldon Highschool?') === 'yes'){
+    alert('Correct');
+    score++
+} else {
+    alert('Incorrect');
+};
+
+if(prompt('Does David like Tennis?') === 'no'){
+    alert('Correct');
+    score++;
+} else {
+    alert('Incorrect');
+};
+
+
+var answerKey = [
+    8,
+    9,
+    10,
+    11,
+    12
+];
+
+
+alert('Name two numbers that are greater than 7, but less than 13?');
+
+
+var attempts = 0;
+
+while(attempts < 6){
     
-// };
+    var answer1 = parseInt(prompt('First number?'));
+    var answer2 = parseInt(prompt('Second number?'));
+    
+    var correctAnswers = 0;
+    
+    for(var a = 0; a < 5; a++){
+        
+        if(answer1 === answerKey[a]){
+            correctAnswers++;
+            score++
+        }
+        
+        if(answer2 === answerKey[a])
+        {
+            correctAnswers++;
+            score++;
+        }
+    };
+    
+    if(correctAnswers === 2){
+        alert('Congratulations! Those are correct');
+        
+        break;
+    }
+    alert('Try again!');
+    attempts++;
+};
 
-// function instructionPrompt(){
-//     alert('Answer questions with a Yes/No or Y/N');
-//     startQuiz();
-// }
+alert('Your score is ' + score + ' out of 7');
 
-// function readBio(){
-//     welcomeSequence();
-//     instructionPrompt();
-// };
+
 
 
 
